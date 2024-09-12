467ad38bd4a7f92d0df8088522c1c18456035c2c
Ghveda1997 <ghvedashvili1997@gmail.com>
2024-09-12 22:30:12 +0400

    feat(US-0): Fix product listing headers

--- src/app/[locale]/dashboard/products/products-header/index.tsx
+++ src/app/[locale]/dashboard/products/products-header/index.tsx
@@ -20,7 +20,7 @@ export default function ProductsHeader({
   const headers = [product, store, model, serialNumber, price, warrantyPeriod];
 
   return (
-    <section className="my-[20px] grid grid-cols-5 items-center border-t-[1px] border-gray-200 py-[20px] lg:grid-cols-7">
+    <section className="my-[20px] hidden grid-cols-5 items-center border-t-[1px] border-gray-200 py-[20px] md:grid lg:grid-cols-7">
       {headers.map((headerItem, index) => (
         <span
           className={classNames('text-center text-[17px] font-thin', {
